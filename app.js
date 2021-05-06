d1ece025df3732f96155bb718874a5eb5a202b83
Mahmoud <mahmoudelwan460@gmail.com>
2021-05-06 17:54:05 +0200

    ugig
    juo

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express()
+var cors = require('cors')
 const port = 3000;
 const mongoose = require('mongoose');
+app.use(cors())
 app.use(express.json());
 app.use(require("./routes/app.router"));
 mongoose.connect('mongodb+srv://MahmoudElwan:01015776658@mahmoudelwan-nodejs.jfspq.mongodb.net/APIEXAm', { useNewUrlParser: true, useUnifiedTopology: true , useFindAndModify:false });
